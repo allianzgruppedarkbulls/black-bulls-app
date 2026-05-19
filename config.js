3a077df8a0adff954b61b308992e4993e7d93571
allianzgruppedarkbulls <allianzgruppe.darkbulls@gmail.com>
2026-05-19 20:39:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Zentrale Konfiguration (keine Secrets hier speichern!)
+// Diese Datei kannst du in GitHub Pages sicher veröffentlichen.
+window.BULLS_CONFIG = {
+  // Google Apps Script WebApp-URL (als API für Login/Chat/Admin)
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwkfI7Z7TW3xRAckzi8Nv3muD6aowlHIxm99RGq76NAmGSplXweLDFa35dgeBK34c5NLA/exec",
+};
